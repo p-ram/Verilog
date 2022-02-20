a2c714dc57c00f94af870e906d23f66d0b1c21a8
p-ram <94652181+p-ram@users.noreply.github.com>
2022-02-20 05:16:47 -0800

    Update declaring_wires.v

--- declaring_wires.v
+++ declaring_wires.v
@@ -1,3 +1,9 @@
+//By default, al the unkwon labels in a verilog file are defined as wires. This bahaviour is very dangerous. Any typo on the signals name will be not detected.
+
+//To solve this, all the verilog files include this command in the beginning:
+`default_nettype none
+
+If the tools detect a signal that has not been previously declared, an error will be shown
 `default_nettype none
 module top_module(
     input a,
