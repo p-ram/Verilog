065a0af849272277c92a496556d3c38b9fff6401
p-ram <94652181+p-ram@users.noreply.github.com>
2022-02-23 02:09:31 -0800

    Update declaring_vector.v

--- declaring_vector.v
+++ declaring_vector.v
@@ -13,3 +13,11 @@ wire [0:7] b;         // 8-bit wire where b[0] is the most-significant bit.
 
 //The endianness ("direction") of a vector is whether the least significant bit has a lower index (little-endian, e.g., [3:0]) or a higher index (big-endian, e.g., [0:3]). 
 //In Verilog, once a vector is declared with a particular endianness, it must always be used the same way. e.g., writing vec[0:3] when vec is declared wire [3:0] vec; is illegal.
+
+w[3:0]      // Only the lower 4 bits of w
+x[1]        // The lowest bit of x
+x[1:1]      // ...also the lowest bit of x
+z[-1:-2]    // Two lowest bits of z
+b[3:0]      // Illegal. Vector part-select must match the direction of the declaration.
+b[0:3]      // The *upper* 4 bits of b.
+assign w[3:0] = b[0:3];    // Assign upper 4 bits of b to lower 4 bits of w. w[3]=b[0], w[2]=b[1],.. etc
